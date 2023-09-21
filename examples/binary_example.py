5c0ee55436d2a7d484b4354d112818b1b2cf70ac
Brandon Karchewski <brandon.karchewski@ucalgary.ca>
2023-09-21 16:25:04 -0600

    modified binary_int, added binary_float, and examples

--- examples/binary_example.py
+++ examples/binary_example.py
@@ -1,12 +1,63 @@
-from goph419.binary import binary_converter
+from goph419.binary import (
+    binary_int,
+    binary_float,
+)
 
 
 def main():
+    # test the Mauricio number
     a = 728226
-    a_str = binary_converter(a)
+    a_str = binary_int(a)
     a_exp = "0 0000000000010110001110010100010"
-    print("test binary_converter()")
-    print(f"value: {a}\nexpected: {a_exp}\nactual  : {a_str}")
+    print(f"test binary_int({a})")
+    print(f"expected: {a_exp}\nactual  : {a_str}\n")
+
+    # test an exact power of 2
+    a = -16
+    a_str = binary_int(a)
+    a_exp = "1 0000000000000000000000000010000"
+    print(f"test binary_int({a})")
+    print(f"expected: {a_exp}\nactual  : {a_str}\n")
+
+    # test a value entered as a binary literal
+    a = 0b01110011001100011000
+    a_str = binary_int(a)
+    a_exp = "0 0000000000001110011001100011000"
+    print(f"test binary_int({a})")
+    print(f"expected: {a_exp}\nactual  : {a_str}\n")
+
+    # test an int providing the (optional) number of bits
+    a = 18
+    b = 11
+    a_str = binary_int(a, b)
+    a_exp = "0 0000010010"
+    print(f"test binary_int({a}, {b})")
+    print(f"expected: {a_exp}\nactual  : {a_str}\n")
+
+    # test an unsigned int, also providing the number of bits
+    a = 18
+    b = 11
+    s = False
+    a_str = binary_int(a, b, s)
+    a_exp = "00000010010"
+    print(f"test binary_int({a}, {b}, {s})")
+    print(f"expected: {a_exp}\nactual  : {a_str}\n")
+
+    # test the Mauricio number as a float
+    a = 728226
+    a_str = binary_float(a)
+    a_exp = ("0 0 0000010100 "
+             + "10110001110010100010000000000000000000000000000000000")
+    print(f"test binary_float({a})")
+    print(f"expected: {a_exp}\nactual  : {a_str}\n")
+
+    # test a negative float that is below 0.5 (i.e. negative exponent)
+    a = -0.01567
+    a_str = binary_float(a)
+    a_exp = ("1 1 0000000101 "
+             + "10000000010111100101111100110000111001111111111101011")
+    print(f"test binary_float({a})")
+    print(f"expected: {a_exp}\nactual  : {a_str}\n")
 
 
 if __name__ == "__main__":
--- src/goph419/binary.py
+++ src/goph419/binary.py
@@ -1,25 +1,92 @@
-def binary_converter(value):
+def binary_int(value, bits=32, signed=True):
     """Convert an integer into a binary string.
 
     Inputs
     ------
     value : int
         The integer to convert
+    bits : int, optional, default=32
+        The number of bits (including the sign bit)
+    signed : bool, optional, default=True
+        Flag for signed or unsigned integer
 
     Returns
     -------
     str
         A string with a binary representation of the input
     """
+    # check that both value and bits are convertible to int
     value = int(value)
-    sign = "0 "
-    if value < 0:
-        sign = "1 "
+    bits = int(bits)
+    # make sure that bits has a sensible value
+    if bits < 1:
+        raise ValueError(f"{bits} is an invalid number of bits, "
+                         + "should be a positive integer")
+    # get the sign bit if it is a signed integer
+    if signed:
+        int_str = "0 "
+        if value < 0:
+            int_str = "1 "
+        bits -= 1
+    else:
+        int_str = ""
+    # build the binary integer string
     rem = abs(value)
-    for b in range(30, -1, -1):
+    for b in range(bits-1, -1, -1):
         if (dig_val := 2 ** b) <= rem:
-            sign += "1"
+            int_str += "1"
             rem -= dig_val
         else:
-            sign += "0"
-    return sign
+            int_str += "0"
+    return int_str
+
+
+def binary_float(value):
+    """Convert a float into a binary string.
+
+    Inputs
+    ------
+    value : float_like
+        The number to convert
+
+    Returns
+    -------
+    str
+        A string with a binary representation of the input
+
+    Notes
+    -----
+    Follows the IEEE-754 standard for a 64-bit float.
+    """
+    # test that the provided value is convertible to float
+    value = float(value)
+    # get the sign information
+    sgn_str = "0"
+    if value < 0:
+        sgn_str = "1"
+    sig = abs(value)
+    # normalize the signficand, determine the exponent
+    exp = 0
+    while sig < 0.5:
+        sig *= 2
+        exp -= 1
+    while sig >= 1.0:
+        sig /= 2
+        exp += 1
+    # get the exponent string, float64 has 11 exponent bits (incl sign bit)
+    exp_str = binary_int(exp, 11)
+    # compute the significand bits, float64 has 53 significand bits
+    # Note: the first bit for 2 **- 1 is not actually stored, it is always 1
+    sig_str = ""
+    rem = sig
+    for b in range(-1, -54, -1):
+        if (dig_val := 2 ** b) <= rem:
+            sig_str += "1"
+            rem -= dig_val
+        else:
+            sig_str += "0"
+    # build the full binary string and return it
+    # Note: the join method of the string class concatenates
+    #       multiple strings together with the character at the beginning
+    #       (in this case a space " ") placed between the strings
+    return " ".join([sgn_str, exp_str, sig_str])
